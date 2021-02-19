a16390c107e386b6f1afe7d5dfe0786bce9e2046
lin <admin@gdgd.jp.net>
2021-02-19 21:02:55 +0900

    #4 モデルを作成
    Minor値をフラグに分解する処理を追加する必要がある

---
+++ lib/model/beacon.dart
@@ -0,0 +1,24 @@
+class ReceivedBeacon{
+  bool isExpanded = false;
+
+  DateTime recvDate;
+  int rawMajor = 0;
+  int rawMinor = 0;
+
+  bool answer0 = false;
+  bool answer1 = false;
+  bool answer2 = false;
+  bool answer3 = false;
+  bool answer4 = false;
+  bool answer5 = false;
+  bool answer6 = false;
+  bool answer7 = false;
+  bool answer8 = false;
+  bool answer9 = false;
+  bool answer10 = false;
+  bool answer11 = false;
+  bool answer12 = false;
+  bool answer13 = false;
+  bool answer14 = false;
+  bool answer15 = false;
+}
