0c4dcfd00af8949e8e2248acf177702561395270
lin <admin@gdgd.jp.net>
2021-02-19 21:30:49 +0900

    #2 展開できるリストの画面を作成
    残：しいたけ回避

--- lib/page/inbox.dart
+++ lib/page/inbox.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:moca/model/beacon.dart';
 
-class InboxPage extends StatefulWidget{
+class InboxPage extends StatefulWidget {
   @override
   _InboxPageState createState() => _InboxPageState();
-
 }
 
-class _InboxPageState extends State<InboxPage>{
-
+class _InboxPageState extends State<InboxPage> {
   void _refreshView() {}
+  List<ReceivedBeacon> _data;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      _data = generateItems(20);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +25,10 @@ class _InboxPageState extends State<InboxPage>{
       appBar: AppBar(
         title: Text("inbox"),
       ),
-      body: Center(
-        child: Text("not implemented"),
+      body: SingleChildScrollView(
+        child: Container(
+          child: _buildPanel(),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _refreshView,
@@ -27,4 +38,61 @@ class _InboxPageState extends State<InboxPage>{
     );
   }
 
+  // リストを作成
+  List<ReceivedBeacon> generateItems(int numberOfItems) {
+    return List.generate(numberOfItems, (int index) {
+      return ReceivedBeacon();
+    });
+  }
+
+  Widget _buildPanel() {
+    return ExpansionPanelList(
+      expansionCallback: (int index, bool isExpanded) {
+        setState(() {
+          _data[index].isExpanded = !isExpanded;
+        });
+      },
+      children: _data.map<ExpansionPanel>((ReceivedBeacon item) {
+        return ExpansionPanel(
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return ListTile(
+              title: Text("20XX年XX月XX日 00:00"),
+                onTap: () {
+                  setState(() {
+                    item.isExpanded = !item.isExpanded;
+                  });
+                }
+            );
+          },
+          body: Padding(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text("質問１　×"),
+                Text("質問２　〇"),
+                Text("質問３　〇"),
+                Text("質問４　×"),
+                Text("質問５　〇"),
+                Text("質問６　×"),
+                Text("質問７　×"),
+                Text("質問８　×"),
+                Text("質問９　×"),
+                Text("質問１０　×"),
+                Text("質問１１　×"),
+                Text("質問１２　×"),
+                Text("質問１３　×"),
+                Text("質問１４　×"),
+                Text("質問１５　×"),
+                Text("質問１６　×"),
+              ],
+            ),
+            padding: const EdgeInsets.all(10.0),
+          ),
+          isExpanded: item.isExpanded,
+        );
+      }).toList(),
+    );
+  }
 }
